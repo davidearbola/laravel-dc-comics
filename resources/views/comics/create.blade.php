b0289fb8cf441e0c8715976e0269e82207056286
davidearbola <davidearbola@gmail.com>
2024-07-05 15:32:53 +0200

    creato form con metodo POST collegato a comics.store

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,14 +3,41 @@
 @section('content')
     <div id="comicCardMain" class="full_box">
         <div class="my_box_container py-3 position-relative">
-            <form action="">
-                <input type="text" placeholder="Titolo fumetto">
-                <input type="text" placeholder="Titolo fumetto">
-                <input type="text" placeholder="Titolo fumetto">
-                <input type="text" placeholder="Titolo fumetto">
-                <input type="text" placeholder="Titolo fumetto">
-                <input type="text" placeholder="Titolo fumetto">
-            </form>
+            <div id="container_form">
+                <h2>Create a new Comic</h2>
+                <form method="POST" action="{{ route('comics.store') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" class="form-control" name="title" id="title">
+                    </div>
+                    <div class="mb-3">
+                        <label for="desc" class="form-label">Description</label>
+                        <input type="text" class="form-control" name="description" id="desc">
+                    </div>
+                    <div class="mb-3">
+                        <label for="thumb" class="form-label">Thumb Path</label>
+                        <input type="text" class="form-control" name="thumb" id="thumb">
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">Price</label>
+                        <input type="text" class="form-control" name="price" id="price">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">Series</label>
+                        <input type="text" class="form-control" name="series" id="series">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">Sale Date</label>
+                        <input type="text" class="form-control" name="sale_date" id="sale_date">
+                    </div>
+                    <div class="mb-3">
+                        <label for="type" class="form-label">Type</label>
+                        <input type="text" class="form-control" name="type" id="type">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
